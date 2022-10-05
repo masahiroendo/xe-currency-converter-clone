ae7e5b60f04081f4eb2357802397815034b46df9
matcha <massahiro.endo@gmail.com>
2022-10-05 15:00:53 +0200

    feat: convert amount by calling exchangeRateApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,72 @@
 import { FC } from "react";
 
-import Converter from "./component/Converter";
+import Converter, {
+  ConversionApiCallFunctionType,
+} from "./component/Converter";
+
+type ExchangeApiConvertResponseData = {
+  motd: {
+    msg: string;
+    url: string;
+  };
+  success: boolean;
+  query: { from: string; to: string; amount: number };
+  info: { rate: number };
+  historical: boolean;
+  date: string;
+  result: number;
+};
+
+// const data = {
+//   motd: {
+//     msg: "If you or your company use this project or like what we doing, please consider backing us so we can continue maintaining and evolving this project.",
+//     url: "https://exchangerate.host/#/donate",
+//   },
+//   success: true,
+//   query: { from: "USD", to: "EUR", amount: 1250 },
+//   info: { rate: 0.846659 },
+//   historical: true,
+//   date: "2021-09-11",
+//   result: 1058.324082,
+// };
+
+const getConversionAmountAndRate: ConversionApiCallFunctionType = async (
+  params
+) => {
+  try {
+    const response = await fetch(
+      `https://api.exchangerate.host/convert?from=${params.from}&to=${params.to}&amount=${params.amount}`,
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        // body: JSON.stringify({ from, to, amount }),
+      }
+    );
+    if (!response.ok) {
+      throw Error("Exchangerate api call failed.");
+    }
+
+    const data: ExchangeApiConvertResponseData = await response.json();
+    console.log(data);
+    return {
+      rate: data.info.rate,
+      result: data.result,
+      date: new Date(data.date),
+      error: null,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      rate: 0,
+      result: 0,
+      date: new Date(),
+      error: "Conversion failed. Reason: " + (error as Error).message,
+    };
+  }
+};
 
 const App: FC = () => {
-  return <Converter />;
+  return <Converter conversionApiCall={getConversionAmountAndRate} />;
 };
 
 export default App;
--- src/component/Converter.tsx
+++ src/component/Converter.tsx
@@ -11,10 +11,26 @@ const currencySymbols = currencies.map((c) => c.symbol);
 
 // type CurrencyType = typeof currencySymbols[number];
 
-const Converter: FC = () => {
+export type ConversionApiCallFunctionType = (params: {
+  from: string;
+  to: string;
+  amount: number;
+}) => Promise<{
+  rate: number;
+  result: number;
+  date: Date;
+  error: string | null;
+}>;
+
+type ConverterProps = {
+  conversionApiCall: ConversionApiCallFunctionType;
+};
+
+const Converter: FC<ConverterProps> = ({ conversionApiCall }) => {
   const [amount, setAmount] = useState<number>(0);
   const [fromCurrency, setFromCurrency] = useState<string>("USD");
   const [toCurrency, setToCurrency] = useState<string>("JPY");
+  const [convertedAmount, setConvertedAmount] = useState<number | null>(null);
 
   const handleAmountChange = (e: ChangeEvent<HTMLInputElement>) => {
     const v = e.target.value;
@@ -43,7 +59,7 @@ const Converter: FC = () => {
     setToCurrency(fromCurrency);
   };
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     if (!checkIfFormIsValid()) {
       return;
@@ -51,6 +67,12 @@ const Converter: FC = () => {
 
     console.log(amount, fromCurrency, toCurrency);
     // call exchange api
+    const { date, error, rate, result } = await conversionApiCall({
+      from: fromCurrency,
+      to: toCurrency,
+      amount: amount,
+    });
+    setConvertedAmount(result);
   };
 
   const checkIfFormIsValid = (): boolean => {
@@ -65,49 +87,55 @@ const Converter: FC = () => {
   const isFormValid = checkIfFormIsValid();
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="amount">Amount</label>
-        <input
-          id="amount"
-          name="amount"
-          value={amount}
-          onChange={handleAmountChange}
-        />
-        <label htmlFor="fromCurrency">From</label>
-        <select
-          name="fromCurrency"
-          id="fromCurrency"
-          value={fromCurrency}
-          onChange={fromCurrencySelectHandler}
-        >
-          {currencies.map((c) => (
-            <option key={c.symbol} value={c.symbol}>
-              {c.text}
-            </option>
-          ))}
-        </select>
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="amount">Amount</label>
+          <input
+            id="amount"
+            name="amount"
+            value={amount}
+            onChange={handleAmountChange}
+          />
+          <label htmlFor="fromCurrency">From</label>
+          <select
+            name="fromCurrency"
+            id="fromCurrency"
+            value={fromCurrency}
+            onChange={fromCurrencySelectHandler}
+          >
+            {currencies.map((c) => (
+              <option key={c.symbol} value={c.symbol}>
+                {c.text}
+              </option>
+            ))}
+          </select>
 
-        <button type="button" onClick={swapCurrency}>
-          {"<- Change ->"}
-        </button>
+          <button type="button" onClick={swapCurrency}>
+            {"<- Change ->"}
+          </button>
 
-        <label htmlFor="toCurrency">To</label>
-        <select
-          name="toCurrency"
-          id="toCurrency"
-          value={toCurrency}
-          onChange={toCurrencySelectHandler}
-        >
-          {currencies.map((c) => (
-            <option key={c.symbol} value={c.symbol}>
-              {c.text}
-            </option>
-          ))}
-        </select>
-        <button disabled={!isFormValid}>Convert</button>
-      </form>
-    </div>
+          <label htmlFor="toCurrency">To</label>
+          <select
+            name="toCurrency"
+            id="toCurrency"
+            value={toCurrency}
+            onChange={toCurrencySelectHandler}
+          >
+            {currencies.map((c) => (
+              <option key={c.symbol} value={c.symbol}>
+                {c.text}
+              </option>
+            ))}
+          </select>
+          <button disabled={!isFormValid}>Convert</button>
+        </form>
+      </div>
+      <div>{} </div>
+      <div>
+        Converted amount: {convertedAmount} {toCurrency}
+      </div>
+    </>
   );
 };
 
