69b00e9ac09a4d9e73147aec1b26b7d8973bbe4c
matcha <massahiro.endo@gmail.com>
2022-11-11 11:20:13 +0100

    feat: integrate tailwind css into project

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import { getConversionAmountAndRate } from "./api/exchangerate-api";
 import Converter from "./components/Converter";
 
 const App: FC = () => {
-  return <Converter conversionApiCall={getConversionAmountAndRate} />;
+  return (
+    <>
+      <div className="h-[25vh] flex justify-center items-center bg-hero">
+        <h1 className="text-3xl">Currency Converter</h1>
+      </div>
+      <Converter conversionApiCall={getConversionAmountAndRate} />
+    </>
+  );
 };
 
 export default App;
