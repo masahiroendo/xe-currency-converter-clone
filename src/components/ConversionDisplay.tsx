9da7cea352161dceafc56c304ba445090acde53f
matcha <massahiro.endo@gmail.com>
2022-11-18 11:02:14 +0100

    feat: display number accounting
    with accounting from "accounting" package added

--- src/components/ConversionDisplay.tsx
+++ src/components/ConversionDisplay.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import accounting from "accounting";
 
 import { ConvertedDataType } from "./types";
 
@@ -19,23 +20,50 @@ const ConversionDisplay: FC<ConversionDisplayProps> = ({ data }) => {
   const fromPrecision: number = data.rate >= 1 ? 8 : 3;
   const toPrecision: number = data.rate >= 1 ? 3 : 8;
 
+  const formatCurrency = (amount: number, text: string): string => {
+    const formattedAmount = accounting.formatMoney(amount, {
+      symbol: text,
+      format: "%v %s",
+      decimal: ".",
+      precision: 2,
+      thousand: ",",
+    });
+    return formattedAmount;
+  };
+
   return (
-    <>
-      <h4>
-        {data.amount} {data.currencyFrom.text} =
-      </h4>
-      <h2>
-        {data.result} {data.currencyTo.text}
-      </h2>
-      <div>
-        1 {data.currencyFrom.symbol} = {data.rate.toFixed(toPrecision)}{" "}
-        {data.currencyTo.symbol}
+    <div className="mt-6">
+      <p className="font-bold text-md text-gray-500 leading-[1.7]">
+        {formatCurrency(data.amount, data.currencyFrom.text)} ={" "}
+      </p>
+      <p className="font-bold text-3xl text-gray-700 leading-[51px] mb-6">
+        {formatCurrency(data.result, data.currencyTo.text)}{" "}
+      </p>
+      <div className="text-sm text-gray-600">
+        <p>
+          1 {data.currencyFrom.symbol} = {data.rate.toFixed(toPrecision)}{" "}
+          {data.currencyTo.symbol}
+        </p>
+        <p>
+          1 {data.currencyTo.symbol} = {(1 / data.rate).toFixed(fromPrecision)}{" "}
+          {data.currencyFrom.symbol}
+        </p>
       </div>
-      <div>
-        1 {data.currencyTo.symbol} = {(1 / data.rate).toFixed(fromPrecision)}{" "}
-        {data.currencyFrom.symbol}
+      <div className="text-xs text-gray-500 flex flex-row-reverse">
+        <p>
+          <>
+            <a href="/#" className="text-blue-500">
+              {data.currencyFrom.text}
+            </a>{" "}
+            to{" "}
+            <a href="/#" className="text-blue-500">
+              {data.currencyTo.text}
+            </a>{" "}
+            conversion — Last update {new Date().toUTCString()}
+          </>
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
